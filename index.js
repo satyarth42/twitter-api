2483caec1aada2bb9a076ef262c66dc2bde62bfd
Satyarth Ojha <satyarth.ojha@gmail.com>
2018-02-19 13:45:21 +0530

    Error handling for empty tracking in streaming

--- index.js
+++ index.js
@@ -4,29 +4,43 @@ var twit = require('twitter');
 var mongoose = require('mongoose');
 var converter = require('json-2-csv')
 
+//add your own credentials for the following fields from your twitter developer account.
 var twitter = new twit({
 	consumer_key: 'Jr5ckrOzJMO6rrAbkdHs1jjvV',
   	consumer_secret: 'NDnJgbgWrCMfONBCskhGo9uMprjSCIqrgbV7e9PyE1dDrFySZf',
  	access_token_key: '964583680348798976-KaPKEAiUGlhDJdgtAyURfRMXNYLJVet',
   	access_token_secret: 'ZJDPPwCzXLsbixnSehLXXTmc6vf2gazARkJpUEkOWfyn5'
 });
+//connecting to mongoose db running on 127.0.0.1:27017 with DB name twitter-api
 mongoose.connect('mongodb://127.0.0.1:27017/twitter-api');
 var stream;
 var filtered_data;
 
+//getting the mongoose models for DB connectivity
 var tweets = require('./models/tweets.js');
 var mentions = require('./models/urls_user_mentions.js');
 var users = require('./models/users.js');
 
+//error handling
+app.get('/error',function(req,res){
+	res.send('error: track query missing');
+});
 
+//api listener for starting the tracking stream.
 app.get('/',function(req,res){
-	var j = {a:1,b:2};
-	res.send(JSON.stringify(j));
+	var track;
+	if(!req.query.track)
+		res.redirect('/error');
+	res.sendStatus(200);
+	//twitter stream created and listening
 	stream = twitter.stream('statuses/filter', {track: req.query.track});
 	stream.on('data', function(event) {
+		//checking for arriving stream
 	  if(event && event.text){
+	  	//checking if a user already exists.
 		  users.find({_id:event.user.id_str},function(err,docs){
 		  	if(docs.length==0){
+		  		//storing some metadata of twitter user in users collection.
 		  		var user_data = new users({
 		  		"_id":event.user.id_str,
 		  		"name":event.user.name,
@@ -40,7 +54,7 @@ app.get('/',function(req,res){
 		  	}
 		  });
 
-
+		  //creating tweet metadata document
 		  var tweet_data = new tweets({
 		  		"_id":event.id_str,
 		  		"text":event.text,
@@ -53,12 +67,15 @@ app.get('/',function(req,res){
 		  		"favorited":event.favorited,
 		  		"jsdate":parseTwitterDate(event.created_at).getTime()
 		  });
+		  //checking if in case tweet alrady exists
 		  tweets.find({_id:event.id_str},function(err,doc){
 		  	if(doc.length==0){
+		  		//storing some tweet metadata in tweets collection
 		  		tweet_data.save(function(err,updated){
 		  		if(err) console.log(err);
 		  		});
 
+		  		//storing urls mentioned in tweets in urls_user_mentions collection
 		  		event.entities.urls.forEach(function(item,index){
 			  	var urls_data = new mentions({
 			  		"tweet_id":event.id_str,
@@ -69,7 +86,7 @@ app.get('/',function(req,res){
 			  		if(err) console.log(err);
 			  	});
 			  });
-
+		  		//storing users mentioned in tweets in urls_user_mentions collection
 		  		event.entities.user_mentions.forEach(function(item,index){
 			  	var user_mentions_data = new mentions({
 			  		"tweet_id":event.id_str,
@@ -89,27 +106,32 @@ app.get('/',function(req,res){
 		  
 	  }
 	});
-	 
+	 //handling for case if there is an error in twitter stream
 	stream.on('error', function(error) {
 	  res.send('error occured');
 	  stream.destroy();
 	});
 });
 
+//api listener for stopping twitter stream.
 app.get('/stop_stream',function(req,res){
 	stream.destroy();
 	res.sendStatus(200);
 });
 
+//api listener for filtering data
 app.get('/filter_data',function(req,res){
 	var language,text,sort_order,sorting_field,start_date,end_date,limit,page,user_name,screen_name,url,user_mention,retweet_min,retweet_max,favorite_min,favorite_max,follower_min,follower_max,friend_min,friend_max;
 	var params = req.query;
+
+	//checking for sort query passed in ajax call
 	if(!params.sort)
 		sort_order=1;
 	else if(params.sort=='-1')
 		sort_order=-1;
 	else
 		sort_order=1;
+	//checking for sorting field query passed in ajax call
 	if(!params.sorting_field)
 		sorting_field="jsdate";
 	else{
@@ -119,17 +141,20 @@ app.get('/filter_data',function(req,res){
 			sorting_field=params.sorting_field;
 	}
 
+	//checking if text searching is required.
 	if(params.text)
 		text = ".*"+params.text+".*";
 	else
 		text = ".*";
 
+	//checking for language filter passed in ajax call
 	if(params.language)
 		language = params.language;
 	else
 		language = ".*";
-
+	//checking for start date filter passed in ajax call
 	if(params.start_date){
+		//date should strictly be in ISO (Date-Time) format
 		start_date = params.start_date;
 		start_date = new Date(start_date);
 		start_date = start_date.getTime();
@@ -137,11 +162,12 @@ app.get('/filter_data',function(req,res){
 	else
 		start_date=0;
 
+	//checking for end date filter passed in ajax call
 	if(params.end_date){
+		//date should strictly be in ISO (Date-Time) format
 		end_date=params.end_date;
 		end_date=new Date(end_date);
 		end_date = end_date.getTime();
-		console.log(end_date);
 	}
 	else
 	{
@@ -149,21 +175,25 @@ app.get('/filter_data',function(req,res){
 		end_date = d.getTime();
 	}
 
+	//checking for pagination limit filter
 	if(params.limit)
 		limit=Number(params.limit);
 	else
 		limit=10;
 
+	//checking for pagination page number filter
 	if(params.page)
 		page=Number(params.page);
 	else
 		page=1;
 
+	//checking for user name filter
 	if(params.user_name)
 		user_name=params.user_name;
 	else
 		user_name=".*";
 
+	//checking for user_name matching filter contains/exact/starts_with/ends_with
 	if(user_name!=".*"){
 		if(params.user_name_type=="contains"){
 			user_name=".*"+user_name+".*";
@@ -176,11 +206,13 @@ app.get('/filter_data',function(req,res){
 		}
 	}
 
+	////checking for screen_name filter
 	if(params.screen_name)
 		screen_name=params.screen_name;
 	else
 		screen_name=".*";
 
+	//checking for screen_name matching filter contains/exact/starts_with/ends_with
 	if(screen_name!=".*"){
 		if(params.screen_name_type=="contains"){
 			screen_name=".*"+screen_name+".*";
@@ -193,11 +225,13 @@ app.get('/filter_data',function(req,res){
 		}
 	}
 
+	//checking for urls filter
 	if(params.url)
 		url=params.url;
 	else
 		url=".*";
 
+	//checking for url matching filter contains/exact/starts_with/ends_with
 	if(url!=".*"){
 		if(params.url_type=="contains"){
 			url=".*"+url+".*";
@@ -210,11 +244,13 @@ app.get('/filter_data',function(req,res){
 		}
 	}
 
+	//checking for user_mentions filter
 	if(params.user_mention)
 		user_mention=params.user_mention;
 	else
 		user_mention=".*";
 
+	//checking for user_mentions matching filter contains/exact/starts_with/ends_with
 	if(user_mention!=".*"){
 		if(params.user_mention_type=="contains"){
 			user_mention=".*"+user_mention+".*";
@@ -227,6 +263,7 @@ app.get('/filter_data',function(req,res){
 		}
 	}
 
+	//checking for retweets range filter
 	if(params.retweet_min)
 		retweet_min = params.retweet_min;
 	else
@@ -242,6 +279,7 @@ app.get('/filter_data',function(req,res){
 		retweet_min = params.retweets;
 	}
 
+	//checking for favorited range filter
 	if(params.favorite_min)
 		favorite_min = params.favorite_min;
 	else
@@ -257,6 +295,7 @@ app.get('/filter_data',function(req,res){
 		retweet_min = params.favorite;
 	}
 
+	//checking for followers range filter
 	if(params.follower_min)
 		follower_min = params.follower_min;
 	else
@@ -272,6 +311,7 @@ app.get('/filter_data',function(req,res){
 		follower_max = params.follower;
 	}
 
+	//checking for friends range filter
 	if(params.friend_min)
 		friend_min = params.friend_min;
 	else
@@ -287,20 +327,25 @@ app.get('/filter_data',function(req,res){
 		friend_max = params.friends;
 	}
 
+	//enabling for only url or only user_mention filter
 	if(url==".*" && user_mention!=".*")
 		url="no-url";
 	else if(url!=".*" && user_mention==".*")
 		user_mention="no-user";
+
+	//finding url and user mentions from urls_user_mentions collection
 	mentions.find(
 		{
 			$or:[
 				{
+					//if url is present according to filtering
 					$and:[
 						{type:'url'},
 						{content:{$regex:url,$options:'i'}}
 					]
 				},
 				{
+					//if user is mentioned according to filtering
 					$and:[
 						{type:'user'},
 						{content:{$regex:user_mention,$options:'i'}}
@@ -311,59 +356,77 @@ app.get('/filter_data',function(req,res){
 	,function(err,docs){
 		if(err) console.log(err);
 		else{
+			//extracting the tweet_ids for url and user mentions.
 			var tweet_id = [];
 			docs.forEach(function(item,index){
 				tweet_id.push(item.tweet_id);
 			});
+			//filtering the user_ids.
 			users.find({
 				$and:[
 					{
+						//filter for user_name
 						name:{$regex:user_name,$options:'i'}
 					},
 					{
+						//filter for screen_name
 						screen_name:{$regex:screen_name,$options:'i'}
 					},
 					{
+						//range filter for followers
 						$and:[{follower_count:{$gte:follower_min}},{follower_count:{$lte:follower_max}}]
 					},
 					{
+						//range filter for friends
 						$and:[{friend_count:{$gte:friend_min}},{friend_count:{$lte:friend_max}}]
 					}
 				]
 			},function(err,user_data){
+				//extracting the user_ids according to filter
 				var user_id = [];
 				user_data.forEach(function(item,index){
 					user_id.push(item._id);
 				});
 				tweets.find({
+					//putting filter on tweets
 					$and:[
 						{
+							//filter on tweet_id if present in url or user mentions
 							_id:{$in:tweet_id}
 						},
 						{
+							//filter if tweet is in given time range
 							$and:[{jsdate:{$lte:end_date}},{jsdate:{$gte:start_date}}]
 						},
 						{
+							//filter for text searching in tweet
 							text:{$regex:text,$options:'i'}
 						},
 						{
+							//language filter
 							language:{$regex:language,$options:'i'}
 						},
 						{
+							//filter on user_id if present in filtered users
 							userid:{$in:user_id}
 						},
 						{
+							//filter on retweet count range
 							$and:[{retweet_count:{$lte:retweet_max}},{retweet_count:{$gte:retweet_min}}]
 						},
 						{
+							//filter on favorited count range
 							$and:[{favorite_count:{$lte:favorite_max}},{favorite_count:{$gte:favorite_min}}]
 						}
 					]
 				},function(err,tweet_data){
 					if(err) console.log(err);
 					else{
+						//sorting data according to column
 						filtered_data = tweet_data.sort(GetSortOrder(sorting_field,sort_order));
+						//pagination
 						filtered_data = filtered_data.slice(limit*(page-1),limit*(page-1)+limit);
+						//sending JSON output
 						res.send(JSON.stringify(filtered_data));
 					}
 				}).populate({path:'userid',select:'_id name screen_name followers_count friend_count followers_count'}).select('-__v');
@@ -373,14 +436,17 @@ app.get('/filter_data',function(req,res){
 	}).select('tweet_id -_id');
 });
 
+//api listener for generating CSV file
 app.get('/get_csv',function(req,res){
 
 
 	var keys;
+	//checkign for keys required in CSV
 	if(!req.query.keys)
 		keys = ['_id','text','userid._id','userid.name','userid.screen_name','userid.friend_count','user.follower_count','retweet_count','favorite_count','language','time','retweeted','favorited'];
 	else
 		keys = req.query.keys.split(',');
+	//options for CSV
 	var options = {
 		'delimiter':{
 			'wrap' : '"',
@@ -396,9 +462,11 @@ app.get('/get_csv',function(req,res){
 	    'keys'             : keys
 	}
 
+	//CSV generation function call
 	converter.json2csv(filtered_data,function(err,csv){
 		if(err) console.log(err);
 		else{
+			//sending the generated SCV as a file
 			res.setHeader('Content-disposition', 'attachment; filename=filtered_data.csv');
   			res.set('Content-Type', 'text/csv');
   			res.status(200).send(csv);
@@ -406,11 +474,13 @@ app.get('/get_csv',function(req,res){
 	},options);
 });
 
+//function for parsing twiitter date format to ISO(Date-Time) format
 function parseTwitterDate(aDate)
 {   
   return new Date(Date.parse(aDate.replace(/( \+)/, ' UTC$1')));
 }
 
+//function for sorting
 function GetSortOrder(prop,sort_order) {  
     return function(a, b) {  
         if (a[prop] > b[prop]) {  
@@ -422,4 +492,5 @@ function GetSortOrder(prop,sort_order) {
     }  
 }  
 
+//server listening on http://127.0.0.1:3000
 app.listen(3000,'127.0.0.1');
